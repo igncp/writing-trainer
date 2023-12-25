aa22afb14d9f5623b4977bd5530c95800d70da70
igncp <icarbajop@gmail.com>
2023-12-25 09:21:18 +0000

    Deploying to gh-pages from @ igncp/writing-trainer@c7bfee40655c7911b4486792cf7f15c495b1455e 🚀

--- web-app/_next/static/qE5p9uwZIDsGCupL94coE/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/css/032fc5e287fba1ae.css","static/chunks/pages/index-e9c8c51a7f660fee.js"],"/_error":["static/chunks/pages/_error-b6491f42fb2263bb.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
