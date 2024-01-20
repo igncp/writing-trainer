ded28c55f2537350cd1ea54a2ecc47f923d6baa3
Ignacio <icarbajop@gmail.com>
2024-01-20 23:08:59 +0800

    新增歌曲並重構歌曲部分程式碼

--- packages/react-ui/src/languages/cantonese/songs/index.ts
+++ packages/react-ui/src/languages/cantonese/songs/index.ts
@@ -1,31 +1,31 @@
-export const songs = [
-  {
-    load: () => import('./kay-tse-saan-lam-dou'),
-    name: '山林道',
-    video: 'W4q4XHhDM-c',
-  },
-  {
-    load: () => import('./kay-tse-nei-mun-dik-hang-fuk'),
-    name: '你們的幸福',
-    video: 'oYgMRIIVX3w',
-  },
-  {
-    load: () => import('./kay-tse-nin-dou-zi-go'),
-    name: '年度之歌',
-    video: 'XAobAFsWTy8',
-  },
-  {
-    load: () => import('./kay-tse-zoi-ngo-zau'),
-    name: '載我走',
-    video: 'wboL_3_StIA',
-  },
-  {
-    load: () => import('./kay-tse-lei-bat-hoi'),
-    name: '離不開',
-    video: 'pNDI9oBG7po',
-  },
-].map(song => ({
-  ...song,
-  artist: 'Kay Tse',
-  lang: 'cantonese',
-}))
+const handleSongTreble =
+  (artist: string, lang: string) =>
+  ([load, name, video]: [
+    () => Promise<{ lyrics: string[] }>,
+    string,
+    string,
+  ]) => ({
+    artist,
+    lang,
+    load,
+    name,
+    video,
+  })
+
+const kayTseSongs = [
+  [() => import('./kay-tse-saan-lam-dou'), '山林道', 'W4q4XHhDM-c'],
+  [() => import('./kay-tse-nei-mun-dik-hang-fuk'), '你們的幸福', 'oYgMRIIVX3w'],
+  [() => import('./kay-tse-nin-dou-zi-go'), '年度之歌', 'XAobAFsWTy8'],
+  [() => import('./kay-tse-zoi-ngo-zau'), '載我走', 'wboL_3_StIA'],
+  [() => import('./kay-tse-lei-bat-hoi'), '離不開', 'pNDI9oBG7po'],
+].map(handleSongTreble('Kay Tse', 'cantonese'))
+
+const myLittleAirportSongs = [
+  [
+    () => import('./my-little-airport-naa-zan-si-bat-zi-dou'),
+    '那陣時不知道',
+    '5xi49OreS3k',
+  ],
+].map(handleSongTreble('My Little Airport', 'cantonese'))
+
+export const songs = kayTseSongs.concat(myLittleAirportSongs)
