021311c3e7a8d3db515fd12e7b4f935a726e392d
Ignacio <icarbajop@gmail.com>
2024-01-24 22:55:22 +0800

    新增兩首新歌完整歌詞

--- packages/react-ui/src/languages/cantonese/songs/index.ts
+++ packages/react-ui/src/languages/cantonese/songs/index.ts
@@ -18,6 +18,7 @@ const kayTseSongs = [
   [() => import('./kay-tse-nin-dou-zi-go'), '年度之歌', 'XAobAFsWTy8'],
   [() => import('./kay-tse-zoi-ngo-zau'), '載我走', 'wboL_3_StIA'],
   [() => import('./kay-tse-lei-bat-hoi'), '離不開', 'pNDI9oBG7po'],
+  [() => import('./kay-tse-faat-jyu-cing'), '法與情', 'qjvAG2Y2LaE'],
 ].map(handleSongTreble('Kay Tse', 'cantonese'))
 
 const myLittleAirportSongs = [
@@ -31,6 +32,11 @@ const myLittleAirportSongs = [
       import('./my-little-airport-nin-hing-dik-caa-cang-ting-lou-baan-loeng'),
     '年輕的茶餐廳老闆娘',
     'dh4eydYk6EA',
+  ],
+  [
+    () => import('./my-little-airport-heoi-seon-wo-maai-dip'),
+    '去信和賣碟',
+    'gQHmVvwagZw',
   ],
 ].map(handleSongTreble('My Little Airport', 'cantonese'))
 
